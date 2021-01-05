25d5be54c896452274dea0c335a1456ae0c63897
Juergen Christ <jchrist@linux.ibm.com>
2021-01-05 13:50:12 +0100

    Change endianness test to allow for failure

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 int main(void)
 {
 	const int x = 1;
-	puts(*((char *)&x) == 1 ? "little endian" : "big endian");
-	return 0;
+	int le = *((char *)&x) == 1;
+	puts(le ? "little endian" : "big endian");
+	return le;
 }
